cbc3c5b61a6bc497e80f0c658542ffeb12429613
kawaegle <alecromski@gmail.com>
2021-11-22 09:23:52 +0100

    create local/bin

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-mkdir -p $HOME/.local/share/template_tek
+mkdir -p $HOME/.local/share/template_tek $HOME/.local/bin
 cp -r rsrc/* $HOME/.local/share/template_tek
 cp template_tek $HOME/.local/bin/
