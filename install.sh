95fed268364ab8d74c10b181387f48b1bb671e27
kawaegle <alecromski@gmail.com>
2021-12-07 10:19:56 +0100

    add lib management

--- install.sh
+++ install.sh
@@ -11,8 +11,11 @@ printf "${green} [+] install template_tek\n"
 cp template_tek $HOME/.local/bin/
 
 printf "${green} [+] get user lib from epitech\n${white}"
-read -p "enter your epitech lib_workshop: " lib_dir
-git clone $lib_dir $HOME/.local/share/template_tek/lib
-rm -rf $HOME/.local/share/template_tek/lib/.git
-
+if [ -d $HOME/.local/share/template_tek/lib/ ]; then
+    (cd $HOME/.local/share/template_tek/lib/ && git pull origin master)
+else
+    read -p "enter your epitech lib_workshop: " lib_dir
+    git clone $lib_dir $HOME/.local/share/template_tek/lib;
+fi
 printf "${green} [!] Install done"
+
