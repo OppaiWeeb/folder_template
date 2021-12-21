6101594cbaa8e9f632f827868781d48ec435fa20
kawaegle <alecromski@gmail.com>
2021-12-21 10:59:19 +0100

    install the git package from git only if not existing

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ green='\033[0;32m'
 white='\033[0;00m'
 
 printf "${green} [+] install ressources on the folder\n"
-git clone https://github.com/OppaiWeeb/folder_template $HOME/.local/share/template_tek 
+if [[ ! -d $HOME/.local/share/template_tek ]]; then
+    git clone https://github.com/OppaiWeeb/folder_template $HOME/.local/share/template_tek 
+fi
 mkdir -p $HOME/.local/bin
 
 printf "${green} [+] install template_tek\n"
